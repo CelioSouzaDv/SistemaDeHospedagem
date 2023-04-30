4951d7d163e0724079b12f61bda970580456ea93
CelioSouzaDv <127158626+CelioSouzaDv@users.noreply.github.com>
2023-04-30 03:38:07 -0300

    novo commit

---
+++ ProjetoHotel/Models/Reserva.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoHotel.Models
+{
+   public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Reserva(Suite suite) 
+        {
+            this.Suite = suite;
+   
+        }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new ArgumentException ("Não foi possivel fazer a reserva, quantidade de hospedes é maior que a capacidade do quarto");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            {
+                int quantidade = Hospedes.Count;
+                return quantidade;
+            }
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor = valor * 0.90M;
+            }
+
+            return valor;
+        }
+    }
+}
